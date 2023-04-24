5967dedf0f041fecdd534807a485063fbfdaf61a
Rodrigo Travessa <rodrigo.travessa@valtech.com>
2023-04-24 13:35:11 -0300

    ContactOPedia started

---
+++ contactopedia/src/Components/Header.jsx
@@ -0,0 +1,9 @@
+ const Header = () => {
+   return (
+    <div style={{borderBottom : "solid"}}>
+        <h1 className="px-5" > Contact O Pedia App </h1>
+    </div>
+   )
+}
+
+export default Header;
