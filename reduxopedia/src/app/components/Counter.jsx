1857b11a939c89083ff4d7836c6490181099b2db
Rodrigo Travessa <rodrigo.travessa@valtech.com>
2023-05-17 12:35:14 -0300

    Redux, calling actions, passing payloads

--- reduxopedia/src/app/components/Counter.jsx
+++ reduxopedia/src/app/components/Counter.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {increment, decrement, multiply, divide} from "../../redux/slice/counterSlice"
 
 function Counter() {
+  // useSelector to get state
+  const count = useSelector((state) => state.counterStore.count)
+  //useDispatch to trigger Slice's actions
+  const dispatch = useDispatch();
+  
+  const [multiplier, setMultiplier] = useState(10);
+
+  const handleInput = () => {
+    setMultiplier(document.getElementById('multiplierInput').value)
+  }
+
+  const handleMultiplier = () => dispatch(multiply(multiplier))
+  const handleDivider = () => dispatch(divide(multiplier))
+      
+    
+  
+
   return (
     <div>
-      Counter : 
+      Counter : {count}
+      <div>
+        <button onClick={() => dispatch(increment())}>+1</button>
+        <button onClick={() => dispatch(decrement())}>-1</button>
+      </div>
+      <div>
+        <input type='text' placeholder={multiplier} id='multiplierInput' defaultValue={multiplier} onChange={handleInput}></input>
+        <button onClick={handleMultiplier}>x</button>
+        <button onClick={handleDivider}>/</button>
+      </div>
     </div>
   )
 }
--- reduxopedia/src/redux/store.js
+++ reduxopedia/src/redux/store.js
@@ -1,9 +1,20 @@
 import {configureStore} from "@reduxjs/toolkit"
+import {counterReducer} from "./slice/counterSlice"
 
 
 export const store = configureStore({
     reducer : {
+        counterStore : counterReducer,
+
 
     },
     
 })
+
+
+//Calling the method using store.dispatch and passing the path to the method.
+// store.dispatch({
+//     type:"counter/increment",
+// })
+// console.log(store.getState());
+
